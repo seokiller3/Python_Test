3e8069447bdbe82db3aa83cdcf8b31ad875a11f5
Artem Kartashev <seokiller2011@gmail.com>
2022-09-17 12:43:37 +0300

    Добавил нахождение четного/нечетного числа

---
+++ C# Семинар/Домашка_3/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+Console.Clear();
+
+Console.Write("Введите число:");
+
+int number = int.Parse(Console.ReadLine());
+
+if (number %2 == 0)
+{
+    Console.WriteLine($"{number} - четное число");
+}
+
+else
+{
+    Console.WriteLine($"{number} - нечетное число");
+}
