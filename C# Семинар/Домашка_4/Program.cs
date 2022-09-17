bd2bbbf64e16e5d3ad65425698d7ab8facd8f64c
Artem Kartashev <seokiller2011@gmail.com>
2022-09-17 13:27:39 +0300

    Добавил решение нахождения четных чисел от 1 до N (не запускается)

---
+++ C# Семинар/Домашка_4/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.Clear
+
+Console.Write("Write number: ");
+
+int n = int.Parse(Console.ReadLine());
+
+int i = 1;
+
+while (i <= n);
+{
+    if (i % 2 == 0)
+{
+    Console.WriteLine(i);
+}
+
+i++;
+
+} 
+
