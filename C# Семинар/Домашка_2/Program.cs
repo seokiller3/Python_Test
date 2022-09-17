aba08d2f7b54564c44f1fa1005c9898b83e3a71c
Artem Kartashev <seokiller2011@gmail.com>
2022-09-17 12:37:47 +0300

    Добавил решение нахождение наибольшего числа из трех значений

---
+++ C# Семинар/Домашка_2/Program.cs
@@ -0,0 +1,43 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Clear();
+
+Console.Write("Введите первое число:");
+
+int number_1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число:");
+
+int number_2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число:");
+
+int number_3 = int.Parse(Console.ReadLine());
+
+if (number_1>number_2)
+{
+    if (number_1>number_3)
+    {
+        Console.WriteLine($"Наибольшее число = {number_1}");
+    }
+}
+
+if (number_2>number_1)
+{
+    if (number_2>number_3)
+    {
+        Console.WriteLine($"Наибольшее число = {number_2}");
+    }
+}
+
+if (number_3>number_2)
+{
+    if (number_3>number_1)
+    {
+        Console.WriteLine($"Наибольшее число = {number_3}");
+    }
+}
