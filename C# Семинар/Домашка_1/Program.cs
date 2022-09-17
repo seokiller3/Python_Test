a10f3aa45d8140a4b6cc7ef80bf4ca4ab18cd834
Artem Kartashev <seokiller2011@gmail.com>
2022-09-17 12:26:37 +0300

    Добавил решение домашки #1

---
+++ C# Семинар/Домашка_1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.Clear();
+
+Console.Write("Введите первое число:");
+
+int number_1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число:");
+
+int number_2 = int.Parse(Console.ReadLine());
+
+if (number_1>number_2)
+{
+    Console.WriteLine($"Наибольшее число = {number_1}");
+}
+
+else
+{
+    Console.WriteLine($"Наибольшее число = {number_2}");
+}
