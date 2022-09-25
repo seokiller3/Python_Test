7751dee140dec5bf90b725225fb5d628a067cb73
Artem Kartashev <seokiller2011@gmail.com>
2022-09-25 17:31:22 +0300

    Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.

--- C# Семинар/5/Program.cs
+++
@@ -1,23 +0,0 @@
-﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа. 456 -> 6 782 -> 2 918 -> 8
-
-
-Console.Clear();
-
-Console.Write("Введите трехзначное число:");
-
-string num = (Console.ReadLine());
-
-Console.WriteLine(num.ToString()[2]);
-
-
-
-Console.WriteLine("Введите трехзначное число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-
-int num_dec = num - (num / 100) * 100;
-int num_l = num_dec - (num_dec / 10) * 10;
-
-Console.WriteLine(num_l);
-
-
-
--- Лекция C# 2/2/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.Clear();
-
-int a = new Random().Next(100, 1000);
-
-Console.WriteLine(a);
-
-int b = (a / 100) * 10 + a % 10;
-
-Console.WriteLine(b);
