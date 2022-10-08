cf0f8ef42e92c987f68ee6e8276b82a642b6c722
Artem Kartashev <seokiller2011@gmail.com>
2022-10-08 22:05:44 +0300

    Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.

---
+++ Семинар C#/Семинар C# 7/2/Program.cs
@@ -0,0 +1,46 @@
+﻿/*Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5 */
+
+Console.Clear();
+
+Console.Clear();
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rowCount = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int columnCount = int.Parse(Console.ReadLine());
+
+int[,] array = FillArray(rowCount, columnCount, 1, 10);
+PrintArray(array);
+
+int[,] FillArray(int rows, int columns, int min, int max)
+{
+    int[,] filledArray = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            filledArray[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return filledArray;
+}
+
+void PrintArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(" [ ");
+            Console.Write(inputArray[i,j] = i + j);
+             Console.Write(" ] ");
+        }
+        Console.WriteLine();
+    }
+}
