fcb0efe118d2ce1628b55e671008cf90d3ee3bb4
Artem Kartashev <seokiller2011@gmail.com>
2022-10-08 22:07:57 +0300

    Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце

---
+++ Семинар C#/Семинар C# 7/HW3/Program.cs
@@ -0,0 +1,56 @@
+﻿/*Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.  */
+
+Console.Clear();
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rowCount = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int columnCount = int.Parse(Console.ReadLine());
+
+
+
+int[,] array = FillArray(rowCount, columnCount, 1, 10);
+PrintArray(array);
+
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double average = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        average = (average + array[i, j]);
+    }
+    average = average / rowCount;
+    Console.Write(average + "; ");
+}
+
+int[,] FillArray(int rows, int columns, int min, int max)
+{
+    int[,] filledArray = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            filledArray[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return filledArray;
+}
+
+void PrintArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(" " + inputArray[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
