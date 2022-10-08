9cf0374f769053b67234d4c46d07ec2f96b46880
Artem Kartashev <seokiller2011@gmail.com>
2022-10-08 13:36:08 +0300

    Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.

---
+++ Семинар C#/Семинар C# 6/3/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+Console.Clear();
+
+Console.WriteLine("Введите размер массива:");
+int arr_size = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = new int [arr_size];
+int [] arr_copy = new int [arr_size];
+
+Random rnd = new Random();
+for (int i = 0; i < arr_size; i++)
+{
+    arr[i] = rnd.Next();
+}
+
+for (int i = 0; i < arr_size; i++)
+{
+    arr_copy[i] = arr[i];
+}
+Console.WriteLine("[" + String.Join(",", arr_copy) + "]");
