4f7431dc12b5d457faeb846b81e7574f3733f7d7
Artem Kartashev <seokiller2011@gmail.com>
2022-10-16 23:56:55 +0300

    Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N с помощью рекурсии

---
+++ Семинар C#/Семинар C# 9/2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N с помощью рекурсии
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int number4 = Convert.ToInt32(Console.ReadLine());
+void PrintMN(int number3, int number4)
+{
+    Console.Write(number3 + ", ");
+    number3++;
+    if (number3 == number4)
+    {
+        Console.Write(number4);
+        return;
+    }
+    else
+    {
+        PrintMN(number3, number4);
+    }
+}
+PrintMN(number3, number4);
