7e6b0571dd7f287288449656eb1d75c18c404e97
Artem Kartashev <seokiller2011@gmail.com>
2022-10-16 23:57:27 +0300

    Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.

---
+++ Семинар C#/Семинар C# 9/4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.Write("Введите А: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите B: ");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine(MyPow(A, B));
+
+int MyPow(int A_l, int B_l)
+{
+    if (B_l == 0) return 1;
+    return A_l * MyPow(A_l, B_l - 1);
+}
